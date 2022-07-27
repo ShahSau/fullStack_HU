07566ac250f09d3e722995072558ce467a64f37c
ShahSau <shahriarksaurov@gmail.com>
2022-07-27 21:06:28 +0300

    1.9: unicafe step4

--- unicafe/src/components/Statistics.js
+++ unicafe/src/components/Statistics.js
@@ -4,12 +4,18 @@ const Statistics=({good,bad,neutral})=> {
   return (
     <>
       <h1>Statistics</h1>
+      {good>0 || bad>0 || neutral>0 
+      ?
+      <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good+neutral+bad}</p>
       <p>average {(good-bad)/(good+neutral+bad)}</p>
       <p>positive {good/(good+neutral+bad)*100}%</p>
+      </>
+      :
+        <p>No feedback given</p>}
     </>
   )
 }
