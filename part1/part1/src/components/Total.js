6f391e9cc5eb4f7eac23ca00b1510dfadfe79d22
ShahSau <shahriarksaurov@gmail.com>
2022-07-27 19:38:07 +0300

    1.4: course information step4

--- part1/part1/src/components/Total.js
+++ part1/part1/src/components/Total.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const Total=({total})=> {
+const Total=({parts})=> {
+  let total=0
+  parts.map(part=>{
+   return total+=part.exercises 
+  })
   return (
+    
     <p>Number of exercises {total}</p>
   )
 }
