8142d3c5ee2ce532cd115309d9386ea12140b078
ShahSau <shahriarksaurov@gmail.com>
2022-07-28 21:08:31 +0300

    2.4: Course information step9

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 const Total=({total})=> {
-  // let total=0
-  // parts.map(part=>{
-  //  return total+=part.exercises 
-  // })
   return (
     
     <p>Number of exercises {total}</p>
