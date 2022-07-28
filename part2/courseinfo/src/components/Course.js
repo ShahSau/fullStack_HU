07c0066bf6d6c43d034a0d62d888c9bc8f444d37
ShahSau <shahriarksaurov@gmail.com>
2022-07-28 20:59:37 +0300

    2.2: Course information step7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Part from './Part'
+import Total from './Total'
 
 const Course=({course})=> {
   return (
     <>
        <Header title={course.name} />
         {course.parts.map(part => <Part key={part.id} part={part.name} exercise={part.exercises} />)}
+        <Total total={course.parts.reduce((acc, curr) => acc + curr.exercises, 0)} />
     </>
   )
 }
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Total=({parts})=> {
-  let total=0
-  parts.map(part=>{
-   return total+=part.exercises 
-  })
+const Total=({total})=> {
+  // let total=0
+  // parts.map(part=>{
+  //  return total+=part.exercises 
+  // })
   return (
     
     <p>Number of exercises {total}</p>
