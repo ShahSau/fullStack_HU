1d2279eec556bf566d1c567b099020dca1aa4988
ShahSau <shahriarksaurov@gmail.com>
2022-08-01 20:58:15 +0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: 123456 }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("");
   const [newNumber, setnewNumber] = useState("");
+  const [change, setChange] = useState("");
 
   const name_array = persons.map((person) => person.name);
 
   const handleNewName = (event) => {
-    // setNewName(event.target.value);
     if (name_array.includes(event.target.value)) {
       alert(`${event.target.value} is already added to phonebook`);
       setNewName("");
@@ -33,9 +38,20 @@ const App = () => {
     setNewName("");
     setnewNumber("")
   };
+
+  const handleChange=(event)=>{
+    setChange(event.target.value)
+  }
+  let update_persons = persons
+
+  if (change !==""){
+    update_persons = update_persons.filter(person => person.name.includes(change))
+  }
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      filter shown with <input onChange={handleChange}/>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -48,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {update_persons.map((person) => (
         <Person key={Math.random()} person={person.name} number ={person.number}/>
       ))}
     </div>
