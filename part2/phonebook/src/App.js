4a96e9458e45073792520533a2fefd501b014514
ShahSau <shahriarksaurov@gmail.com>
2022-08-09 19:10:21 +0300

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState,useEffect } from "react";
 import Filter from "./components/Filter";
 import { Form } from "./components/Form";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
   const [newName, setNewName] = useState("");
   const [newNumber, setnewNumber] = useState("");
   const [change, setChange] = useState("");
-
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+    .then(response => {
+      setPersons(response.data);
+    }).catch(error => {
+      console.log(error);
+    })
+  }, []);
   const name_array = persons.map((person) => person.name);
 
   const handleNewName = (event) => {
